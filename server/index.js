a886013f48c6d9db323fb206e460a84a8ca281a4
lolo <bloren641@gmail.com>
2024-10-19 20:23:40 -0300

    intento 3 conexion bd
    Profes en componentes tuvimos inconvenientes para hacer el commit, lo subimos por aca

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const app = express();
+const mysql= require ('mysql');
+const cors= require('cors');
+
+app.use(cors());
+
+let db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '10203040',
+    database: 'mydb'
+})
+
+app.get('/', (req, res) => {
+    db.query("INSERT INTO 'mydb'.'Profesion' ('nombre') VALUES('Ingeniería de prueba')", (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send('Error en la base de datos');
+        } else {
+            console.log(result);
+            res.send('Datos insertados correctamente');
+        }
+    });
+});
+
+
+app.listen(8080, ()=>{
+    console.log('server escuchando puerto 8080');
+})
